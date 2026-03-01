a3f3d7c0da8aecc6afe7c21e1a48474f7bb6339e
Imasha_SK_2003 <imashakumarasiri1603@gmail.com>
2026-03-01 14:20:25 +0530

    Correct studentFirstName typo and enhance output
    Fix typo in variable name and update print statements.

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
         double studentGPA = 3.45;
         String studentFirstName = "Kayla";
         String studentLastName = "Hammon";
+        System.out.println(studentFirstname + " " + studentLastName)
+        System.out.println(studentFirstName + " " + studentLastName + " " + " has got a GPA of " + studentGPA)
     }
 }
